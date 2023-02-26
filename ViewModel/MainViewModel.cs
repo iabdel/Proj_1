01b37f5741bb7b9aac98cbef71f98b449f60d327
iabdel <isam.abdelhameed@outlook.com>
2023-02-26 18:13:58 +0300

    Init

---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿
+using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace MauiApp1.ViewModel;
+
+public partial class MainViewModel : ObservableObject
+{
+    public MainViewModel()
+    {
+        items = new ObservableCollection<string>();
+    }
+    [ObservableProperty]
+    ObservableCollection<string> items;
+
+    [ObservableProperty]
+    string text;
+
+    [ICommand]
+    void Add()
+    {
+        if(string.IsNullOrEmpty(Text))
+            return;
+        Items.Add(Text);
+        Text = string.Empty;
+    }
+    [ICommand]
+    void Delete(string s)
+    {
+        if(Items.Contains(s))
+        {
+            Items.Remove(s);
+        }
+    }
+    [ICommand]
+    async Task Tap(string s)
+    {
+        await Shell.Current.GoToAsync($"{nameof(DetailPage)}?Text={s}");
+    }
+}
